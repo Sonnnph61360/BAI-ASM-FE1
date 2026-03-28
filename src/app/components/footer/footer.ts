16864017073ddbbd7c57dbc15f595b2d2e90ffd5
Sonnnph61360 <supebrolyzz@gmail.com>
2026-03-28 08:00:07 +0700

    Refactor footer component: update HTML structure, add footer sections, and integrate CommonModule

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,9 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+  footerSections = [
+    {
+      title: 'Company',
+      links: ['About', 'Features', 'Works', 'Career']
+    },
+    {
+      title: 'Help',
+      links: ['Customer Support', 'Delivery Details', 'Terms & Conditions', 'Privacy Policy']
+    },
+    {
+      title: 'FAQ',
+      links: ['Account', 'Manage Deliveries', 'Orders', 'Payments']
+    },
+    {
+      title: 'Resources',
+      links: ['Free eBooks', 'Development Tutorial', 'How to - Blog', 'Youtube Playlist']
+    }
+  ];
+
+  payments = ['visa.png', 'mastercard.png', 'paypal.png', 'apple-pay.png', 'google-pay.png'];
+}
